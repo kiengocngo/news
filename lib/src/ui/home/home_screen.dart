c382e365ecaf5db9ef63556c5f680ef917aaa18a
KienNgo <kienngo@KienNgos-MacBook-Pro.local>
2022-08-15 08:25:55 +0700

    first commit

---
+++ lib/src/ui/home/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news/theme/news_colors.dart';
+import 'package:news/theme/news_theme_data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: NewsColor.textWhite,
+        centerTitle: true,
+        title: Text(
+          'HOME',
+          style: NewsThemeData.fromContext(context).textAppBar,
+        ),
+      ),
+    );
+  }
+}
